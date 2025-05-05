b33d24c48a8824651ed342474182b9b213199db6
Ely <152279955+Ellisia-Chan@users.noreply.github.com>
2025-05-05 21:39:09 +0800

    Add files via upload

---
+++ EventBus.cs
@@ -0,0 +1,58 @@
+using System;
+using System.Collections.Generic;
+
+namespace Core.EventSystem {
+    public static class EventBus {
+        // Dictionary storing ordered lists of delegates for each event type
+        private static readonly Dictionary<Type, List<Delegate>> eventTable = new();
+
+        // Subscribe to an event (both class and struct supported)
+        public static void Subscribe<T>(Action<T> callback) {
+            var type = typeof(T);
+
+            if (!eventTable.TryGetValue(type, out var delegateList)) {
+                delegateList = new List<Delegate>();
+                eventTable[type] = delegateList;
+            }
+
+            delegateList.Add(callback);
+        }
+
+        // Unsubscribe from an event
+        public static void UnSubscribe<T>(Action<T> callback) {
+            var type = typeof(T);
+
+            if (eventTable.TryGetValue(type, out var delegateList)) {
+                delegateList.Remove(callback);
+
+                // Clean up if list is empty
+                if (delegateList.Count == 0) {
+                    eventTable.Remove(type);
+                }
+            }
+        }
+
+        // Unsubscribe all listeners of a given event type
+        public static void UnsubscribeAll<T>() {
+            eventTable.Remove(typeof(T));
+        }
+
+        // Clear all events
+        public static void ClearAll() {
+            eventTable.Clear();
+        }
+
+        // Publish an event to all listeners
+        public static void Publish<T>(T evt) {
+            var type = typeof(T);
+
+            if (eventTable.TryGetValue(type, out var delegateList)) {
+                // Clone list to prevent modification during iteration
+                var listeners = delegateList.ToArray();
+                foreach (var del in listeners) {
+                    ((Action<T>)del)?.Invoke(evt);
+                }
+            }
+        }
+    }
+}
